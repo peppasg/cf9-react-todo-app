1c68f3bd26350dac2759b4c88337927642706575
johnny <dark_skateboarding2@hotmail.com>
2026-05-20 21:00:37 +0300

    feat(todo): add useTodos hook

---
+++ src/features/todo/hooks/useTodos.ts
@@ -0,0 +1,60 @@
+import {useState} from "react";
+import type {Todo} from '../types.ts'
+
+export const useTodos = () => {
+    // STATE TODOS
+    // [
+    //             {id:3242426, text: text1, completed: false},
+    //             {id:3453463, text: text2, completed: true},
+    //             {id:5645634, text: text3, completed: false},
+    // ]
+
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    const addTodo = (text: string) => {
+        setTodos(prev => [
+            ...prev,
+            {id:Date.now(), text: text, completed: false},
+            ])
+    }
+
+
+    const deleteTodo = (id: number) => {
+        setTodos(
+            prev => prev.filter(todo => todo.id != id)
+        )
+    }
+
+    const editTodo = (id: number, newText: string) => {
+        setTodos(
+            prev =>
+                prev.map(todo =>
+                todo.id === id ? {...todo, text: newText} : todo
+                )
+        )
+    }
+
+
+    const toggleTodo = (id: number) => {
+        setTodos(
+            prev =>
+                prev.map(todo =>
+                    todo.id === id ? {...todo, completed: !todo.completed} : todo
+                )
+        )
+    }
+
+    const clearAll = () => {
+        setTodos([]);
+    }
+
+
+    return {
+        todos,
+        addTodo,
+        deleteTodo,
+        editTodo,
+        toggleTodo,
+        clearAll,
+    }
+}
